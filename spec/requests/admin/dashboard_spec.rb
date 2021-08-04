61c1e241fe112c658ce260c2d370d6c8f54a1da7
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-04 15:41:08 +0300

    Test for CircleCI rspec pipeline

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe "Dashboard", type: :request do
         get '/admin/dashboard'
       end
 
+      it 'shows a flash warning to user' do
+        expect(flash[:alert]).to be_present
+      end
+
       it 'redirects to home page' do
         expect(response).to redirect_to('/')
       end
 
       it "it returns 302 code" do
         expect(response.status).to eq(302) 
-      end
-
-      it 'shows a flash warning to user' do
-        expect(flash[:alert]).to be_present
       end
     end
 
