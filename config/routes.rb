b2cb1fc0ab71e15f540a7e8fa37858c83a969846
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-05 14:34:53 +0300

    Add custom fields to devise registration form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  devise_for :users
-  
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  resources :users
   namespace :admin do
     get '/dashboard', to: "dashboard#index"
   end
