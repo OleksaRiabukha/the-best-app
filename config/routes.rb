56cc1153c03fbd2b7b1c8e5a71f867e5bc8fea5e
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-06 15:59:39 +0300

    Add blank lines in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  root 'home#index'
   devise_for :users
+
   resources :users
+  
   namespace :admin do
     get '/dashboard', to: "dashboard#index"
   end
+
+  root 'home#index'
 end
