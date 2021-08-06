705944b2b403718b2ebed1e8ef40399513d9060a
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-06 16:44:45 +0300

    Add test for validations on restaurant model

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -18,5 +18,8 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name)}
+  end
 end
