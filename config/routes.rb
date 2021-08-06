0ab5102e616151138a307c1cc32fa58dac8bb60d
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-06 15:35:33 +0300

    Add routes to restaurants to users and admins

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   resources :users
+
+  resources :restaurants
+
   namespace :admin do
     get '/dashboard', to: "dashboard#index"
+    resources :restaurants
   end
 end
