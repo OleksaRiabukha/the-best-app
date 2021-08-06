e1ddafab2e43a7dc73f64baba094fdfbcc1285d4
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-06 14:29:58 +0300

    Change data type of phone number in users table from int to text

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@
 #  encrypted_password     :string           default(""), not null
 #  first_name             :string
 #  last_name              :string
-#  phone_number           :integer
+#  phone_number           :text
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
