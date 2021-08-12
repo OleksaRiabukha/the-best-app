8855610db248c0944ca1d599a55a2652df53dcae
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-12 17:42:04 +0300

    Remove spaces

--- spec/requests/admin/dashboard_spec.rb
+++ spec/requests/admin/dashboard_spec.rb
@@ -21,11 +21,11 @@ RSpec.describe 'Dashboard', type: :request do
       it 'shows a flash warning to user' do
         expect(flash[:alert]).to be_present
       end
-    
+
       it 'redirects to home page' do
         expect(response).to redirect_to('/')
       end
-    
+
       it 'returns 302 code' do
         expect(response).to have_http_status(:found) 
       end
