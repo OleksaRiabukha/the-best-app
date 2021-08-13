6882d5f1aed152ce12ea3aa60e83670f1d0c8b7d
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-13 11:25:47 +0300

    Make Faker restaurant names unique

--- spec/factories/restaurant.rb
+++ spec/factories/restaurant.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :restaurant do
-    name { Faker::Restaurant.name }
+    name { Faker::Restaurant.unique.name }
     phone_number { Faker::PhoneNumber.phone_number }
     website_url { Faker::Internet.url }
     description { Faker::Restaurant.description }
