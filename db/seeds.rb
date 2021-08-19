753dcced9bb44b658683793653c24fae4d066b12
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-08-19 15:39:48 +0300

    Add each to FactoryBot in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Restaurant.destroy_all
 FactoryBot.create_list(:user, 2)
 FactoryBot.create_list(:restaurant, 2, active: false)
 
-FactoryBot.create_list(:restaurant, 2, active: true) do |restaurant|
+FactoryBot.create_list(:restaurant, 2, active: true).each do |restaurant|
   FactoryBot.create_list(:menu_item, 2, available: true, restaurant: restaurant)
   FactoryBot.create_list(:menu_item, 2, available: false, restaurant: restaurant)
 end
