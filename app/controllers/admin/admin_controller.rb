243ae07a78600da005e355d1b128f6756494b553
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-09-13 12:34:19 +0300

    Remove margin bottom from navbar, add layout for admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,6 +1,8 @@
 class Admin::AdminController < ApplicationController
   before_action :require_admin
 
+  layout 'admin'
+
   private
 
   def require_admin
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,14 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  def after_sign_in_path_for(resource)
+    if current_user.admin?
+      admin_dashboard_path
+    else
+      root_path
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
