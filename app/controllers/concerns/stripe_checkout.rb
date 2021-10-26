c39dfb612fd087c81c9b792238364f450cc08687
OleksaRiabukha <oleksa.wheat@gmail.com>
2021-10-26 16:39:54 +0300

    Fix styling

--- app/controllers/concerns/stripe_checkout.rb
+++ app/controllers/concerns/stripe_checkout.rb
@@ -2,20 +2,24 @@ module StripeCheckout
   def self.create_stripe_checkout(amount, payment_for, success_url, cancel_url, current_user, model)
     amount = (amount.to_d.round(2) * 100).to_i
 
-    Stripe::Checkout::Session.create({ customer: current_user.stripe_customer_id,
-                                       payment_method_types: [:card],
-                                       line_items: [{
-                                         name: 'Total price for the order:',
-                                         amount: amount,
-                                         currency: 'usd',
-                                         quantity: 1
-                                       }],
-                                       payment_intent_data: {
-                                         metadata: { payment_for: payment_for, model: model.to_json }
-                                       },
-                                       mode: 'payment',
-                                       success_url: success_url,
-                                       cancel_url: cancel_url + '?stripe_session_id={CHECKOUT_SESSION_ID}' })
+    Stripe::Checkout::Session.create(
+      {
+        customer: current_user.stripe_customer_id,
+        payment_method_types: [:card],
+        line_items: [{
+          name: 'Total price for the order:',
+          amount: amount,
+          currency: 'usd',
+          quantity: 1
+        }],
+        payment_intent_data: {
+          metadata: { payment_for: payment_for, model: model.to_json }
+        },
+        mode: 'payment',
+        success_url: success_url,
+        cancel_url: cancel_url + '?stripe_session_id={CHECKOUT_SESSION_ID}' 
+      }
+    )
   end
 
   def self.cancel_payment_intent(session_id)
